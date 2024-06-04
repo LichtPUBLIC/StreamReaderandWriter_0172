064eec39680fd878fba31cb8cc0035d5422650c5
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-06-04 10:30:37 +0700

    membuat int main dan menambahkan deklarasi

--- Pertemuan13/Pertemuan13.cpp
+++ Pertemuan13/Pertemuan13.cpp
@@ -1,6 +1,19 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    // untuk menyimpan data berbentuk string
+    string baris;
+
+    // membuka file dalam mode menulis
+    ofstream outfile;
+
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+   
